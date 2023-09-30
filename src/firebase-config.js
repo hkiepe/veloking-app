f8701a11b1fafa93ed355df73ab9548413d50450
Henrik Kiepe <hkiepe@ssc-services.de>
2023-09-30 13:23:50 +0200

    firebase config added databaseURL

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,6 +5,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
