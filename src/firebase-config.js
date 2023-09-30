6405ba510e8adfb5b339abe3eb27fca50650415c
Henrik Kiepe <drgoodcat77@googlemail.com>
2023-09-30 11:10:26 +0000

    added .env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,23 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-/* const firebaseConfig = {
-  // TODO create a .ENV file to secure my data
-  apiKey: "AIzaSyBrmJySbDW85r8z_G3GNsMkVlktcJNC9nY",
-  authDomain: "yield-backend.firebaseapp.com",
-  projectId: "yield-backend",
-  storageBucket: "yield-backend.appspot.com",
-  messagingSenderId: "647360894787",
-  appId: "1:647360894787:web:8bd96ec957365a497a41f6",
-}; */
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCLqQ90QElqBR9uY4rLRGEFS6R3MlwThGQ",
-  authDomain: "veloking-backend.firebaseapp.com",
-  projectId: "veloking-backend",
-  storageBucket: "veloking-backend.appspot.com",
-  messagingSenderId: "379515048574",
-  appId: "1:379515048574:web:9a379b399adf8866b17e2f",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
